68f1bf9b7f16b65db1ffbf940a8bc7d0858d40d4
hoyishian <ysho2@illinois.edu>
2018-09-12 12:03:37 -0500

    Done

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,7 +35,20 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while (!maze.isFinished()) {
+                maze.turnLeft();
+                if (maze.canMove()) {
+                    maze.move();
+                }
+                else {
+                    while (!maze.canMove()) {
+                        maze.turnRight();
+                    }
+                    maze.move();
+                }
+            }
         }
+
 
         if (maze.isFinished()) {
             System.out.println("You solved the maze!");
